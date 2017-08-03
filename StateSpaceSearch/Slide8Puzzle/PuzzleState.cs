f36b8ee14533ebdc30d5fe08886d5b9a3fc63735
Sushil Singh <thesushil@gmail.com>
2017-08-03 09:19:43 -0500

    Added 3rd problem - solve  8 piece sliding puzzle

---
+++ StateSpaceSearch/Slide8Puzzle/PuzzleState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using StateProblemCommons;
+
+namespace Slide8Puzzle
+{
+    public class PuzzleState : AbstractState, IState
+    {
+        public int[][] Board { get; set; }
+
+        public bool IsGoal()
+        {
+            var rowMax = Board.GetLength(0) - 1;
+            var colMax = Board.GetLength(1) - 1;
+            var numInBoard = 0;
+            for (var i = 0; i <= rowMax; i++)
+            for (var j = 0; j <= colMax; j++)
+                if (Board[i][j] != ++numInBoard && i != rowMax && j != colMax) return false;
+            return true;
+        }
+
+        public IEnumerable<IState> GetNextStates()
+        {
+            //var emptySlot = FindEmptySlot();
+            //emptySlot
+        }
+
+        //private (int, int) FindEmptySlot()
+        //{
+        //    for(int i = 0; i <= Board.GetLength(0); i++)
+        //    for (int j = 0; j <= Board.GetLength(1); j++)
+        //    {
+        //        if (Board[i][j] == 0) return (i, j);
+        //    }
+        //}
+    }
+}
